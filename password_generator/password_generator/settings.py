14227e03f12d8c0dbffb035e632d06c4bba97b40
Himanshu Monat <hmonat1996@gmail.com>
2021-06-18 20:30:52 +0530

    removing secret key from code

--- password_generator/password_generator/settings.py
+++ password_generator/password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'd)0%70%kc3tcuk7ne%f*rglx@+@s@w@58%o3vd9!2*9ucq2w*w'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
